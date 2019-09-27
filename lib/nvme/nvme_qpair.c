ea01ab9beb55dc8e75a1ba7de911690bcd9e234f
virgilshi <2338052892@qq.com>
2019-09-27 18:07:15 +0800

    SPDK_NVME_SCT_GENERIC
    Signed-off-by: virgilshi <2338052892@qq.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -563,7 +563,7 @@ nvme_qpair_submit_request(struct spdk_nvme_qpair *qpair, struct nvme_request *re
 					child_req->cmd.cdw12 = 16 - 1;   
 					rc = nvme_qpair_submit_request(qpair, child_req);
 					child_req->cmd.cdw10 += 16;  /////////////// update the lba of next child child io
-					if (child_req->cpl.status.sct == SPDK_NVME_SCT_COMMAND_SPECIFIC) {  
+					if (child_req->cpl.status.sct == SPDK_NVME_SCT_GENERIC) {  
 						while (child_req->cpl.status.sc != SPDK_NVME_SC_SUCCESS) {} //////// wait and issue next io until current io is completed.
 					}
 					
