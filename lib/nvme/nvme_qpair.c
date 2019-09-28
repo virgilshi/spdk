b9fca4cc2d677ae8ba08247882e9748f7e49baee
virgilshi <2338052892@qq.com>
2019-09-28 10:53:52 +0800

    fix-9
    Signed-off-by: virgilshi <2338052892@qq.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -561,7 +561,7 @@ nvme_qpair_submit_request(struct spdk_nvme_qpair *qpair, struct nvme_request *re
 
 #ifdef HUST_SPILT_IO 			/////////// sl 
 			if (child_req->cmd.cdw12 + 1 == 64) ///////////////// child req size is up to 64 (giant for OCSSD), split it into four child io further
-			for (int i = 0; i < 3; ++i) {	//////////// three of child child are proceeded, left one of which is done in below, 
+			for (int i = 0; i < 1; ++i) {	//////////// three of child child are proceeded, left one of which is done in below, 
 											/////////////////in order to keep compatitle with existing framework
 				if (spdk_likely(!child_req_failed)) {
 					child_req->cmd.cdw12 = 16 - 1;   
